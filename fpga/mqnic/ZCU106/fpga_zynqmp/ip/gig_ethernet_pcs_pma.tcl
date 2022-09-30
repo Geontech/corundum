9edb65426c009c1a9d3e21e3acedb8747058b9b5
Joel Palmer <jpalmer@geontech.com>
2022-09-30 16:06:44 -0400

    Add gig_ethernet ip with shared logic, symbolic link to eth_xcvr_phy_1g

--- fpga/mqnic/ZCU106/fpga_zynqmp/ip/gig_ethernet_pcs_pma.tcl
+++ fpga/mqnic/ZCU106/fpga_zynqmp/ip/gig_ethernet_pcs_pma.tcl
@@ -1,14 +1,11 @@
 create_ip -name gig_ethernet_pcs_pma -vendor xilinx.com -library ip -version 16.2 -module_name gig_ethernet_pcs_pma_0
 
 set_property -dict [list \
-    CONFIG.EMAC_IF_TEMAC {TEMAC} \
-    CONFIG.Standard {1000BASEX} \
-    CONFIG.TransceiverControl {true} \
-    CONFIG.Auto_Negotiation {false} \
-    CONFIG.Physical_Interface {Transceiver} \
     CONFIG.Management_Interface {false} \
-    CONFIG.RefClkRate {125} \
+    CONFIG.Auto_Negotiation {false} \
+    CONFIG.SupportLevel {Include_Shared_Logic_in_Core} \
+    CONFIG.TransceiverControl {true} \
+    CONFIG.RefClkRate {156.25} \
     CONFIG.DrpClkRate {62.5} \
-    CONFIG.RxGmiiClkSrc {TXOUTCLK} \
 ] [get_ips gig_ethernet_pcs_pma_0]
 
